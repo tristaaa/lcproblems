e603adf2aca3cd3ccf152f4d3e81ed6a3cf39653
tristaaa <tristaaa56@126.com>
2019-05-10 21:28:18 -0700

    two sum

---
+++ twosum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """ Solution: One-pass Hash Table,
+            While we iterate and inserting elements into the table, 
+            we also look back to check 
+            if current element's complement already exists in the table
+
+            Input: an array of integers
+            Output: One unique list of indicies of two numbers such that their summation is equal to the target
+            
+            ** Notice: Assume that the result will be unique,
+            we can use one element only once, 
+            thus we must return two different indicies of the numbers in the input array 
+        """
+        dd = {}
+        for i in range(len(nums)):
+            numj = target - nums[i]
+            if numj in dd and i!=dd[numj]:
+                return [i, dd[numj]]
+            dd[nums[i]] = i
+         
