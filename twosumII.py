d9072e93841760370c7f8cdf7459811902cafca8
tristaaa <tristaaa56@126.com>
2019-05-11 14:29:17 -0700

    two sum sorted array

---
+++ twosumII.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+            find the unique index(not zero-based) tuple 
+            where the corresponding numbers add up to target number
+            
+            :type numbers: List[int], a sorted array
+            :type target: int
+            :rtype: List[int]
+        """
+        low,high=1,len(numbers)
+        while low<high:
+            tot=numbers[low-1]+numbers[high-1]
+            if tot<target:
+                low+=1
+            elif tot>target:
+                high-=1
+            else:
+                return [low,high]
